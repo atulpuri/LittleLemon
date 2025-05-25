0a94a86b549e2e04d082756ca2e669dd73f4d65e
Atul Puri <atul-puri@hotmail.com>
2025-05-25 21:49:29 +0530

    Update settings.py added drf spectacular

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "djoser",
     #"debug_toolbar",
     "django_filters",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -149,4 +150,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Little Lemon Restauraunt REST API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
 }
